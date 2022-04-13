baf9850bd812858f66edeedef4345e4e35997b40
Beyonder12 <fajri.illahi1211@gmail.com>
2022-04-13 14:32:53 +0700

    goecho

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"gopkg.in/go-playground/validator.v9"
 )
 
 func main() {
@@ -17,7 +18,17 @@ func main() {
 	}
 
 	e := echo.New()
+	v := validator.New()
 	products := []map[int]string{{1: "mobiles"}, {2: "tv"}, {3: "laptops"}}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Well, hello there!!")
+	})
+
+	e.GET("/products", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, products)
+	})
+
 	e.GET("/products/:id", func(c echo.Context) error {
 		var product map[int]string
 		for _, p := range products {
@@ -36,6 +47,32 @@ func main() {
 		}
 		return c.JSON(http.StatusOK, product)
 	})
+
+	e.POST("/products", func(c echo.Context) error {
+		type body struct {
+			Name string `json:"product_name" validate:"required,min=4"`
+			// Vendor          string `json:"vendor" validate:"min=5,max=10"`
+			// Email           string `json:"email" validate:"required_with=Vendor,email"`
+			// Website         string `json:"website" validate:"url"`
+			// Country         string `json:"country" validate:"len=2"`
+			// DefaultDeviceIP string `json:"default_device_ip" validate:"ip"`
+		}
+		var reqBody body
+		if err := c.Bind(&reqBody); err != nil {
+			return err
+		}
+		if err := v.Struct(reqBody); err != nil {
+			return err
+		}
+
+		product := map[int]string{
+			len(products) + 1: reqBody.Name,
+		}
+
+		products = append(products, product)
+		return c.JSON(http.StatusOK, product)
+	})
+
 	e.Logger.Print(fmt.Sprintf("Listening on port %s", port))
 	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
 }
